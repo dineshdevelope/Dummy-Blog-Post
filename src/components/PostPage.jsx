32b8378d7f99e403e55182c2288ffdf3f0c012e3
Dinesh S <dineshindevelope@gmail.com>
2024-10-04 18:26:54 +0530

    button-ui-change

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -34,7 +34,7 @@ const PostPage = () => {
           {post.body}
         </p>
         <Link
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mb-3"
           to={"/"}
         >
           Back
